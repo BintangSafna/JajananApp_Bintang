04bb5f4d1a90dc04ef6e9c9c58253ffac97d3a6c
Bintang Safna <95611250+BintangSafna@users.noreply.github.com>
2024-11-15 00:40:36 +0700

    Update jajananData.kt

--- app/src/main/java/com/bangkit/jajananapp_bintang/data/model/jajananData.kt
+++ app/src/main/java/com/bangkit/jajananapp_bintang/data/model/jajananData.kt
@@ -7,14 +7,14 @@ object jajananData {
             1,
             "Kue Apem",
             2000,
-            "https://id.wikipedia.org/wiki/Berkas:Apem_Side_View.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Kue_apem.jpg/800px-Kue_apem.jpg",
             "Apam atau apem (dikenal juga dengan appam di negeri asalnya India) adalah makanan yang terbuat dari tepung beras yang didiamkan semalam dengan mencampurkan telur, santan, gula dan tape serta sedikit garam kemudian dibakar atau dikukus. Bentuknya mirip serabi, tetapi lebih tebal"
         ),
         Jajanan(
             2,
             "Arem-arem",
             1000,
-            "https://id.wikipedia.org/wiki/Arem-arem",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Arem-arem.JPG/800px-Arem-arem.JPG",
             "Arem-arem adalah panganan berupa nasi yang berisi sayuran atau sambal goreng, kemudian dibungkus dengan daun pisang. Arem-arem populer sebagai makanan pengganti sarapan juga termasuk jajanan pasar yang digemari masyarakat."
         ),
         Jajanan(
@@ -28,84 +28,84 @@ object jajananData {
             4,
             "Clorot",
             1000,
-            "https://id.wikipedia.org/wiki/Berkas:Cerorot.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Cerorot.jpg/800px-Cerorot.jpg",
             "Clorot, celorot, cerorot, atau jelurut adalah kue tradisional berbahan tepung beras dengan santan, dibungkus dengan janur atau kelapa daun kelapa muda dalam bentuk kerucut. Kue ini adalah kudapan tradisional yang biasa ditemukan di Indonesia, Malaysia dan Brunei."
         ),
         Jajanan(
             5,
             "Cenil",
             3000,
-            "https://id.wikipedia.org/wiki/Cenil",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Cenil.JPG/800px-Cenil.JPG",
             "Cenil atau Cetil adalah penganan yang dibuat dari adonan tepung kanji dan gula pasir, diberi pewarna, dibentuk menjadi bulatan-bulatan kecil, dimakan dengan kelapa parut."
         ),
         Jajanan(
             6,
-            "Comro",
+            "Combro",
             2000,
-            "https://id.wikipedia.org/wiki/Combro",
+            "https://upload.wikimedia.org/wikipedia/id/c/cf/Combro.jpg?20140406192509",
             "Comro merupakan gorengan khas Sunda.. Comro terbuat dari parutan singkong yang dibentuk bulat atau lonjong dan diisi dengan sambal oncom dan cabai lalu digoreng."
         ),
         Jajanan(
             7,
             "Dodol",
             10000,
-            "https://id.wikipedia.org/wiki/Dodol",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Dodol_Bangka.JPG/800px-Dodol_Bangka.JPG",
             "Dodol adalah penganan yang dibuat dari tepung ketan, santan kelapa, dan gula merah, kadang-kadang dicampur dengan buah-buahan, seperti durian, sirsak dibungkus daun (jagung), kertas, dan sebagainya. Penganan ini berasal dari Ponorogo, Jawa Timur."
         ),
         Jajanan(
             8,
             "Dadar Gulung",
             2000,
-            "https://id.wikipedia.org/wiki/Dadar_gulung",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/8/81/Dadar_gulung.jpg/450px-Dadar_gulung.jpg?20071028215356",
             "Kue dadar gulung merupakan makanan khas Indonesia dan Malaysia yang dapat digolongkan sebagai panekuk yang diisi dengan parutan kelapa yang dicampur dengan gula jawa cair. Isi ini disebut unti. Kulit dadar gulung identik dengan warna hijau karena diberi pewarna daun suji."
         ),
         Jajanan(
             9,
             "Emping",
             1500,
-            "https://id.wikipedia.org/wiki/Emping",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Emping.jpg/800px-Emping.jpg",
             "Emping adalah sejenis camilan atau makanan ringan Indonesia berupa kerupuk yang terbuat dari biji melinjo atau belinjo. Emping memiliki rasa sedikit pahit. Emping tersedia di pasaran dalam berbagai varian rasa, seperti polos (asli), asin, pedas atau manis"
         ),
         Jajanan(
             10,
             "Getuk",
             35000,
-            "https://id.wikipedia.org/wiki/Getuk",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Getuk_1.jpg/600px-Getuk_1.jpg",
             "Getuk adalah penganan dibuat dari singkong dan sebagainya yang direbus, kemudian dicampur gula pasir atau gula merah dan kelapa ditumbuk sekaligus."
         ),
         Jajanan(
             11,
             "Klepon",
             2500,
-            "https://id.wikipedia.org/wiki/Klepon",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Klepon.JPG/800px-Klepon.JPG",
             "Kelepon dikenal di luar Jawa dengan nama onde-onde, atau kalalapun/kelelepon adalah sejenis kue tradisional khas Indonesia yang termasuk ke dalam kelompok jajanan pasar. Klepon diduga berasal dari Jawa, tetapi dikenal pula secara meluas di wilayah-wilayah di Indonesia"
         ),
         Jajanan(
             12,
             "Kue Lapis",
             1000,
-            "https://id.wikipedia.org/wiki/Kue_lapis",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Kue_Lapis.jpg/800px-Kue_Lapis.jpg",
             "Kue lapis adalah makanan khas Indonesia. Kue ini biasanya terdiri dari dua warna yang berlapis-lapis, ini yang memberi nama kue ini. Kue ini dibuat dari tepung beras, tepung kanji, santan, gula pasir, garam dan pewarna."
         ),
         Jajanan(
             13,
             "Kue Putu",
             15000,
-            "https://id.wikipedia.org/wiki/Kue_putu",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Kue_putu_7.JPG/800px-Kue_putu_7.JPG",
             "Kue putu adalah jenis kudapan tradisional Indonesia berupa kue dengan isian gula jawa, dibalut dengan parutan kelapa, dan tepung beras butiran kasar. Kue ini di kukus dengan diletakkan di dalam tabung bambu yang sedikit dipadatkan."
         ),
         Jajanan(
             14,
             "Lemper",
             3000,
-            "https://id.wikipedia.org/wiki/Lemper",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Lemper.jpg/800px-Lemper.jpg",
             "Lemper adalah penganan yang dibuat dari ketan dan dimasak dengan santan, di dalamnya diisi daging cincang atau abon, dibungkus dengan daun pisang menyerupai lontong tetapi dengan ukuran yang lebih kecil."
         ),
         Jajanan(
             15,
             "Lupis",
             7000,
-            "https://id.wikipedia.org/wiki/Lupis_(makanan)",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Lupis.jpg/800px-Lupis.jpg",
             "Lupis merupakan makanan khas Indonesia terutama daerah Jawa. Ada dua bentuk lupis yang umum ditemui, yakni berbentuk segitiga atau bulat memanjang seperti lontong. Lupis terbuat dari beras ketan yang dimasak, lalu dibungkus dengan daun pisang."
         ),
     )
